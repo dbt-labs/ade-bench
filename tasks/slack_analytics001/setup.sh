0126ab133a86afd0c1b3a328ef3aa82cbe4266ac
Maja Cernja <maja.cernja@krakentechnologies.ltd>
2026-03-06 15:41:33 +0000

    Update setup

--- tasks/slack_analytics001/setup.sh
+++ tasks/slack_analytics001/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-dbt deps
-
-# Makes sure fct table doesn't get created in the db
-dbt run --select +dim_slack_messages 
-
 # Remove the facts folder and all its contents
 rm -rf models/facts
+
+dbt deps
+dbt run
